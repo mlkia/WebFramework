694b25e11e35a0cabbf0ee16055aa74d0a2c7e85
Malik <mlk.2plus@gmail.com>
2022-04-06 11:23:58 +0200

    Used async in UseEffect

--- React/Use Effect/UseEffect/src/components/API.jsx
+++ React/Use Effect/UseEffect/src/components/API.jsx
@@ -4,19 +4,26 @@ import '../App.css'
 
 
 function API () {
-const [posts, setPosts] = useState ("")
-const [gets, setGets] = useState("")
+	const [posts, setPosts] = useState ("")
+	const [gets, setGets] = useState("")
 
-const data = useEffect(() => {
-	fetch("https://api.chucknorris.io/jokes/random")
-	.then(response => response.json())
-	.then(data => {setPosts(data)})
-	})
+	useEffect(() => {
+		const fetchData = async () => {
+			const response  = await fetch('https://api.chucknorris.io/jokes/random');
+			const data = await response.json()
+			// const response = awet fetch("https://api.chucknorris.io/jokes/random")
+			setGets(data)
+
+			// .then(response => response.json())
+			// .then(data => {setPosts(data)})
+	}
+		fetchData()
+})
 
 	return (
 		<div>
-			<button onClick={() => setGets(posts)}>Get the data</button>
-			<h1>{gets.value}</h1>
+			<button onClick={() => setPosts(gets)}>Get the data</button>
+			<h1>{posts.value}</h1>
 		</div>
 	)
 }
